39fea26ba5728ead83d8d8c7a8de58c8a286140f
Alexandr Zykov <118713839+Vass80@users.noreply.github.com>
2023-05-03 12:54:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-user_token = 'vk1.a.1_rpqyKFP6Ez3YNSh-BII8LhdCMMoNIaDuXdPTs4H-EwJPRvHGNF6zKrkeTA7WYjwGmMbHDmEj9RCKemQo68oDlCEXsbUc-J1XWfQOdSwx0wtCSkDE7wkak4RZh1b78i9b-B6NNfvNoOTdQi2yLTtSrebtRVOQlwTwQq47DXygDWPVe1cnPfus6JFCsE7vsT8Hm4nD1LS4crkmwqy6hu4A' # токен пользователя
-comm_token = 'vk1.a.nxsyrkMQlqfkE-Dz5K36Em0FUEcbDsBcoD8gHaRFF5_s_Lfwwe3gC5OnFPLq1tDG7L3N-GbVrL7YmzmgdJozgc-IiJAdol1zSEv16QQgtDNNI3L_W_giQZ7yyXAxE_fDXdiM3RQVWuvRDXrXQ0QUVJa7JcgwsUx1HUTATlP1txS2Ur81n_bAJK7t2MzF5CFqZhgN3ssxpQHaiWGJ5m5rhQ' # токен сообщества
+user_token = 'emQo6'
+comm_token = '1Q' 
 db_url_object = "postgresql+psycopg2://postgres:0@localhost/bot_users"
 
 offset = 0 #сдвиг
